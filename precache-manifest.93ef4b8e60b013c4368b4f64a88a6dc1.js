a98ea17b85c607785708b0a662684e3ea022bfff
Zymantas Katinas <kyssaz@gmail.com>
2020-05-11 07:58:16 +0300

    Updates

---
+++ precache-manifest.93ef4b8e60b013c4368b4f64a88a6dc1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91af26f06659518d9b7b3cc33cd5e909",
+    "url": "/front-end-task/index.html"
+  },
+  {
+    "revision": "bce1e18ab1780d25b460",
+    "url": "/front-end-task/static/css/main.f6eaa092.chunk.css"
+  },
+  {
+    "revision": "450305050f4f8ddbf3bc",
+    "url": "/front-end-task/static/js/2.faf61832.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/front-end-task/static/js/2.faf61832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce1e18ab1780d25b460",
+    "url": "/front-end-task/static/js/main.b9181e00.chunk.js"
+  },
+  {
+    "revision": "6bd154f0011b899c83d7",
+    "url": "/front-end-task/static/js/runtime-main.ea01e2a3.js"
+  },
+  {
+    "revision": "c9740e734c203ba4c16ebe122d7c2b67",
+    "url": "/front-end-task/static/media/alert.c9740e73.svg"
+  },
+  {
+    "revision": "0ca9794f1b4e2d64565e3db6c61bfc5b",
+    "url": "/front-end-task/static/media/arrow-sort.0ca9794f.svg"
+  },
+  {
+    "revision": "8e5b64eeb9d1b9ed5d859ff421d27be6",
+    "url": "/front-end-task/static/media/arrow.8e5b64ee.svg"
+  },
+  {
+    "revision": "ac170fcdfb30f823b199527076e429a7",
+    "url": "/front-end-task/static/media/nordpass-logo.ac170fcd.svg"
+  },
+  {
+    "revision": "d3221164e5da7af7cdfe710d3fa98709",
+    "url": "/front-end-task/static/media/np-form-image.d3221164.png"
+  },
+  {
+    "revision": "0cc6b81f3c351f9a4e38e1f4d1a1e6ef",
+    "url": "/front-end-task/static/media/np-hero-image.0cc6b81f.png"
+  }
+]);
